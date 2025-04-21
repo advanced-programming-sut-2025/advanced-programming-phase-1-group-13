575c16fdccb92ff9066b4f633e6da1ac267c5c26
Selma-Rzv <s.rezavand.1385@gmail.com>
2025-04-21 16:58:28 +0330

    Updated

--- models/User.java
+++ models/User.java
@@ -4,7 +4,8 @@ import models.enums.SecurityQuestion;
 import models.enums.Skill;
 import models.enums.SkillLevel;
 import models.enums.environment.Direction;
-import models.enums.types.Food;
+import models.enums.types.FoodType;
+import models.enums.types.Gender;
 import models.tools.Tool;
 
 import java.util.ArrayList;
@@ -12,6 +13,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class User {
+    private String username;
+    private String password;
+    private String nickname;
+    private String email;
+    private Gender gender;
     private int energy;
     private boolean isEnergyUnlimited;
     private Position position;
@@ -19,7 +25,7 @@ public class User {
     private HashMap<Skill, SkillLevel> SkillLevels;
     private ArrayList<Farm> farms;
     private ArrayList<CraftRecipe> learntCraftRecipes;
-    private ArrayList<Food> learntCookingRecipes;
+    private ArrayList<FoodType> learntCookingRecipes;
     private Map<SecurityQuestion, String> qAndA;
     private Farm farm;
     private ArrayList<Transaction> transactions;
@@ -71,7 +77,7 @@ public class User {
         return null;
     }
 
-    public void LearnNewCraftRecipe(Food craftRecipe) {
+    public void LearnNewCraftRecipe(FoodType craftRecipe) {
         // TODO
     }
 
@@ -89,5 +95,33 @@ public class User {
 
     public void setQAndA(Map<SecurityQuestion, String> qAndA) {
         this.qAndA = qAndA;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
