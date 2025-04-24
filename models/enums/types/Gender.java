cec76c1c63918f0c57afc26d16e50e80583c1e84
arvin <arvinrasulzadeh@gmail.com>
2025-04-24 15:39:47 +0330

    add getGenderByName()

--- models/enums/types/Gender.java
+++ models/enums/types/Gender.java
@@ -4,4 +4,21 @@ public enum Gender {
     WOMAN,
     MAN,
     RATHER_NOT_SAY;
+
+    public static Gender getGenderByName(String genderName) {
+        genderName = genderName.toLowerCase();
+        switch (genderName) {
+            case "man":
+            case "m":
+            case "male":
+                return MAN;
+            case "woman":
+            case "w":
+            case "female":
+            case "f":
+                return WOMAN;
+            default:
+                return RATHER_NOT_SAY;
+        }
+    }
 }
