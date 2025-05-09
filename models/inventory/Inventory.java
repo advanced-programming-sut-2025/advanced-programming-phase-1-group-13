678c0a8a2decd803aec78f16ae5980ee0c1b9413
arvin <arvinrasulzadeh@gmail.com>
2025-05-09 10:49:42 +0330

    removeFromInventory() in Inventory Class

--- models/inventory/Inventory.java
+++ models/inventory/Inventory.java
@@ -1,6 +1,7 @@
 package models.inventory;
 
 import models.Item;
+import models.Result;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,18 +21,43 @@ public abstract class Inventory {
     }
 
     public void addToInventory(Item item, int n) {
-        // TODO
+        items.put(item, n);
     }
 
     public void CheatAddToInventory(Item item, int n) {
         // TODO
     }
 
-    public void removeFromInventory(Item item, int n) {
-        // TODO
+    public Result removeFromInventory(Item item, Integer n) {
+        if (!items.containsKey(item)) {
+            return new Result(false, "Item does not exist.");
+        }
+        if (n == null) {
+            items.remove(item);
+            return new Result(true, "Item removed from inventory.");
+        }
+        int currentQuantity = items.get(item);
+        if (currentQuantity < n) {
+            String message = "The provided number is larger than the quantity in inventory." + "\n" +
+                    "You have " + currentQuantity + " " + item + " in your inventory.";
+            return new Result(false, message);
+        }
+        int newQuantity = currentQuantity - n;
+        if (newQuantity == 0) {
+            items.remove(item);
+            return new Result(true, "Item removed from inventory.");
+        } else {
+            items.put(item, newQuantity);
+            String message = n + " of item <" + item + "> has been removed from inventory." + "\n" +
+                    "You now have " + newQuantity + " of that item in your inventory.";
+            return new Result(true, message);
+        }
     }
 
     public String showItemsInInventory() {
+        if (items.isEmpty()) {
+            return "Your inventory is empty.";
+        }
         StringBuilder sb = new StringBuilder();
         for (Item item : items.keySet()) {
             sb.append(item.toString());
@@ -42,7 +68,7 @@ public abstract class Inventory {
             sb.append("capacity: ").append(capacity).append("\n")
                     .append("empty spots left: ").append(emptyLeft);
         } else {
-            sb.append("your inventory has unlimited capacity!");
+            sb.append("Your inventory has unlimited capacity!");
         }
         return sb.toString();
     }
