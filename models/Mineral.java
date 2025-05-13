60b422f99da6d972cfbce52793ce31c0430dacde
arvin <arvinrasulzadeh@gmail.com>
2025-05-13 12:58:06 +0330

    Mineral extends Item!

--- models/Mineral.java
+++ models/Mineral.java
@@ -3,7 +3,7 @@ package models;
 import models.enums.types.MineralType;
 import models.farming.ForagingStuff;
 
-public class Mineral implements ForagingStuff {
+public class Mineral extends Item implements ForagingStuff {
     private int sellPrice;
     private MineralType mineralType;
     private Position position;
