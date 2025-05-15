3a7e568edcad602961d7cb5874b10038c3d16450
arvin <arvinrasulzadeh@gmail.com>
2025-05-15 14:39:16 +0330

    useTool() Scythe

--- src/main/java/models/tools/Scythe.java
+++ src/main/java/models/tools/Scythe.java
@@ -4,8 +4,11 @@ import models.Tile;
 import models.User;
 import models.enums.Skill;
 import models.enums.SkillLevel;
+import models.enums.types.TileType;
 import models.enums.types.ToolMaterial;
 import models.enums.types.ToolType;
+import models.farming.Crop;
+import models.farming.Tree;
 
 import java.util.HashMap;
 
@@ -30,7 +33,23 @@ public class Scythe extends Tool {
 
     @Override
     public void useTool(Tile tile, User player) {
-
-        super.useTool(tile, player);
+        int energyNeeded = calculateEnergyNeeded(player.getSkillLevels(), player.getCurrentTool());
+        if (energyNeeded >= player.getEnergy()) {
+            player.faint();
+            return;
+        }
+        player.decreaseEnergyBy(energyNeeded);
+        if (tile.getType() == TileType.GRASS) {
+            tile.setType(TileType.NOT_PLOWED_SOIL);
+        } else if (tile.getType() == TileType.TREE) {
+            Tree tree = (Tree) tile.getItemPlacedOnTile();
+            // todo: harvest
+        } else if (tile.getType() == TileType.GROWING_CROP) {
+            Crop crop = (Crop) tile.getItemPlacedOnTile();
+            // todo: harvest
+        } else if (tile.getType() == TileType.GREENHOUSE) {
+            // todo
+            return;
+        }
     }
 }
