c885c0f41c0e5323bf8d190cf92ee590d6f59da5
arvin <arvinrasulzadeh@gmail.com>
2025-05-15 15:29:05 +0330

    fillWateringCan()

--- src/main/java/models/tools/WateringCan.java
+++ src/main/java/models/tools/WateringCan.java
@@ -11,12 +11,37 @@ import models.enums.types.ToolType;
 import java.util.HashMap;
 
 public class WateringCan extends Tool {
+    private int remainingWater;
+    private int waterCapacity;
+
     public WateringCan() {
         super(ToolType.WATERING_CAN);
     }
 
     public WateringCan(ToolMaterial material) {
         super(ToolType.WATERING_CAN, material);
+        this.waterCapacity = switch (material) {
+            case BASIC -> 40;
+            case COPPER -> 55;
+            case IRON -> 70;
+            case GOLD -> 85;
+            case IRIDIUM -> 100;
+        };
+        this.remainingWater = this.waterCapacity;
+    }
+
+
+    public int getRemainingWater() {
+        return this.remainingWater;
+    }
+
+    public int getWaterCapacity() {
+        return this.waterCapacity;
+    }
+
+
+    public void fillTheWateringCan() {
+        this.remainingWater = this.waterCapacity;
     }
 
     @Override
@@ -39,9 +64,8 @@ public class WateringCan extends Tool {
         }
         player.decreaseEnergyBy(energyNeeded);
         if (tile.getType() == TileType.WATER) {
-            // todo: filling up the Watering Can with water
-
-            return;
+            WateringCan wateringCan = (WateringCan) player.getCurrentTool();
+            wateringCan.fillTheWateringCan();
         } else if (tile.getType() == TileType.TREE ||
                 tile.getType() == TileType.PLANTED_SEED ||
                 tile.getType() == TileType.GROWING_CROP) {
