9f73c81c2dd6228bb2364817a7303619743f9f2a
arvin <arvinrasulzadeh@gmail.com>
2025-05-16 15:19:29 +0330

    omitted extra todo

--- src/main/java/models/inventory/Refrigerator.java
+++ src/main/java/models/inventory/Refrigerator.java
@@ -1,13 +1,7 @@
 package models.inventory;
 
-import models.Item;
-
 public class Refrigerator extends Inventory {
     public Refrigerator() {
         super(Integer.MAX_VALUE, true);
-    }
-
-    private void pick(Item item) {
-        // TODO
     }
 }
