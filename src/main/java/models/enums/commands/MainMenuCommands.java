198467c78609a69eb27977f02bd207b844115989
arvin <arvinrasulzadeh@gmail.com>
2025-05-18 03:04:48 +0330

    exit command for Main Menu fixed

--- src/main/java/models/enums/commands/MainMenuCommands.java
+++ src/main/java/models/enums/commands/MainMenuCommands.java
@@ -3,9 +3,9 @@ package models.enums.commands;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public enum MainMenuCommands implements Command{
+public enum MainMenuCommands implements Command {
     USER_LOGOUT("^\\s*(user\\s+)?logout\\s*$"),
-    MENU_EXIT("^\\s*menu\\s+exit\\s*$"),
+    MENU_EXIT("^\\s*(menu)?\\s*exit\\s*$"),
     SHOW_CURRENT_MENU("\\s*show\\s+current\\s+menu\\s*"),
     MENU_ENTER("^\\s*menu\\s+enter\\s+(?<newMenu>.+)\\s*$");
 
