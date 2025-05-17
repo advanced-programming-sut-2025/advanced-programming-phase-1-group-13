d4a82da6dc3209bb7bda3f55170842617f8be662
arvin <arvinrasulzadeh@gmail.com>
2025-05-17 12:19:23 +0330

    updated ToolMaterial

--- src/main/java/models/enums/types/ToolMaterial.java
+++ src/main/java/models/enums/types/ToolMaterial.java
@@ -1,9 +1,31 @@
 package models.enums.types;
 
 public enum ToolMaterial {
-    BASIC,
-    COPPER,
-    IRON, 
-    GOLD,
-    IRIDIUM;
+    BASIC(0, null, 5),
+    COPPER(2000, IngredientType.COPPER_BAR, 5),
+    IRON(5000, IngredientType.IRON_BAR, 5),
+    GOLD(10000, IngredientType.GOLD_BAR, 5),
+    IRIDIUM(25000, IngredientType.IRIDIUM_BAR, 5);
+
+    private final int upgradePrice;
+    private final IngredientType ingredientForUpgrade;
+    private final int numOfIngredientNeededForUpgrade;
+
+    ToolMaterial(int upgradePrice, IngredientType ingredientForUpgrade, int numOfIngredientNeededForUpgrade) {
+        this.upgradePrice = upgradePrice;
+        this.ingredientForUpgrade = ingredientForUpgrade;
+        this.numOfIngredientNeededForUpgrade = numOfIngredientNeededForUpgrade;
+    }
+
+    public int getUpgradePrice() {
+        return this.upgradePrice;
+    }
+
+    public IngredientType getIngredientForUpgrade() {
+        return this.ingredientForUpgrade;
+    }
+
+    public int getNumOfIngredientNeededForUpgrade() {
+        return this.numOfIngredientNeededForUpgrade;
+    }
 }
