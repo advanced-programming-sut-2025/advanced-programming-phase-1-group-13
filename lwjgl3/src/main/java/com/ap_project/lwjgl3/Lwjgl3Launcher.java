2dc5d0bbe7c22a40bfcea591ab4cb583d7d47bb8
Dorsa <zabetidor@gmail.com>
2025-07-26 18:24:01 +0330

    cooking assets

--- lwjgl3/src/main/java/com/ap_project/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/ap_project/lwjgl3/Lwjgl3Launcher.java
@@ -3,7 +3,7 @@ package com.ap_project.lwjgl3;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.ap_project.Main;
-
+//
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
     public static void main(String[] args) {
