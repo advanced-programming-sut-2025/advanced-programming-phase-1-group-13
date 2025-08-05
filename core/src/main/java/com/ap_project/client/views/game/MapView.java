44df064dfdd0a35ae8ec0ea6df5f5dbea3750813
Farrokhi <syfarrokhi2@gmail.com>
2025-08-05 11:41:36 +0330

    Map

--- core/src/main/java/com/ap_project/client/views/game/MapView.java
+++ core/src/main/java/com/ap_project/client/views/game/MapView.java
@@ -3,19 +3,26 @@ package com.ap_project.client.views.game;
 import com.ap_project.Main;
 import com.ap_project.common.models.App;
 import com.ap_project.common.models.GameAssetManager;
+import com.ap_project.common.models.Position;
+import com.ap_project.common.models.User;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+import java.util.ArrayList;
 
 import static com.ap_project.client.views.game.GameMenuView.hoverOnImage;
 
 public class MapView implements Screen, InputProcessor {
     private Stage stage;
     private final Image map;
+    private final ArrayList<Image> players;
+    private final ArrayList<Vector2> farmPositions;
     private final Image closeButton;
     private final GameView gameView;
 
@@ -25,6 +32,17 @@ public class MapView implements Screen, InputProcessor {
             (Gdx.graphics.getWidth() - map.getWidth()) / 2,
             (Gdx.graphics.getHeight() - map.getHeight()) / 2
         );
+
+        players = new ArrayList<>();
+        for (User player : App.getCurrentGame().getPlayers()) {
+            players.add(new Image(GameAssetManager.getGameAssetManager().getPlayerIcon(player.getGender())));
+        }
+
+        this.farmPositions = new ArrayList<>();
+        farmPositions.add(new Vector2(830, 730));
+        farmPositions.add(new Vector2(580, 540));
+        farmPositions.add(new Vector2(830, 350));
+        farmPositions.add(new Vector2(1080, 540));
 
         this.closeButton = new Image(GameAssetManager.getGameAssetManager().getCloseButton());
         closeButton.setPosition(
@@ -40,6 +58,17 @@ public class MapView implements Screen, InputProcessor {
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(this);
         stage.addActor(map);
+
+        for (int i = 0; i < players.size(); i++) {
+            Image player = players.get(i);
+            Position playerPosition = App.getCurrentGame().getPlayers().get(i).getPosition();
+            player.setPosition(
+                farmPositions.get(i).x + 160 * playerPosition.getX()/ 74f,
+                farmPositions.get(i).y - 160 * playerPosition.getY()/ 74f
+            );
+            stage.addActor(player);
+        }
+
         stage.addActor(closeButton);
     }
 
@@ -93,6 +122,8 @@ public class MapView implements Screen, InputProcessor {
             Main.getMain().setScreen(gameView);
             return true;
         }
+
+        System.out.println(screenX + " " + screenY);
         return false;
     }
 
@@ -120,5 +151,4 @@ public class MapView implements Screen, InputProcessor {
     public boolean scrolled(float amountX, float amountY) {
         return false;
     }
-
 }
